bfbde92bbafc58567b85eb039fe0178e6a0ad115
Adrien Gautier <adr.gautier@gmail.com>
2017-01-18 20:09:22 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+  $.getJSON("https://rawgit.com/wooorm/gemoji/master/index.json",function(data){
+   
+    var values = [];
+    Object.keys(data).map(function(symbol, index) {
+      var tmp = data[symbol];
+      
+      var category = tmp.category,
+        tags = tmp.tags,
+        names = [];
+      
+      tmp.names.forEach(function(name){
+        names.push(':'+name+':');
+      })
+
+      values.push({
+        symbol: emojione.unicodeToImage(symbol),
+        category: category,
+        names: names,
+        tags: tags
+      });
+    });
+
+    var FJS = FilterJS(values, '#gemoji-list',{
+      filter_on_init: true,
+      template: '#gemoji-template',
+      search: {ele: '#searchbox', fields: ['category','names','tags']},
+    });
+
+    new Clipboard('.name', {
+        text: function(trigger) {
+            return $.trim($(trigger).text());
+        }
+    });
+  });
+});
